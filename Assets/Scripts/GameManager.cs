0a38182569de0dc4bed2720e291496a02cd18655
Romano3D <romanodez@gmail.com>
2025-10-23 10:54:56 -0300

    add menu and interation the game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using Ebac.Core.Singleton;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("References")]
+    public Transform startPoint;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+
+    private GameObject _currentPlayer;
+
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawPlayer();
+    }
+
+    private void SpawPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From().SetDelay(delay);
+}
+
+
+}
---
+++ Assets/Scripts/Utils/LoadScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void loadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    public void loadScene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+}
