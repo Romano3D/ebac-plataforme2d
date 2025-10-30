aaea5fede8c074692ef78bd30842e47b597a277e
Romano3D <romanodez@gmail.com>
2025-10-30 09:20:06 -0300

    add AnimSatellite

---
+++ Assets/Scripts/AnimatorTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorTest : MonoBehaviour
+{
+    public Animator animator;
+
+    public KeyCode keyToTrigger = KeyCode.A;
+    public string triggerToPlay = "FlyBool";
+
+    private void OnValidate()
+    {
+        if(animator != null) animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToTrigger))
+        {
+            animator.SetBool(triggerToPlay, !animator.GetBool(triggerToPlay));
+        }
+    }
+}
